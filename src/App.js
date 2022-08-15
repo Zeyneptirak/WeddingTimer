ac4802ca93ea0259b98dee61d601272151f78005
Zeyneptirak <zeyneptirak33@gmail.com>
2022-08-15 15:44:03 +0300

    güncelleme

--- src/App.js
+++ src/App.js
@@ -17,9 +17,9 @@ function App() {
       const now = new Date().getTime();
       const distance = countDownDate - now;
       const Days= Math.floor(distance/(24*60*60*1000));
-      const Hours= Math.floor(distance/(24*60*60*1000)/(1000*60*60));
-      const Minutes= Math.floor(distance%(60*60*1000)/(1000*60));
-      const Seconds= Math.floor(distance%(60*1000)/(1000));
+      const Hours= Math.floor((distance%(24*60*60*1000))/(1000*60*60));
+      const Minutes= Math.floor((distance%(60*60*1000))/(1000*60));
+      const Seconds= Math.floor((distance%(60*1000))/(1000));
 
       if(distance<0){
         //Stop Timer
